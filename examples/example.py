a73dcf07aa6428c32064cefb78050d3752236c9c
Vicent Marti <tanoku@gmail.com>
2011-01-02 05:47:25 +0200

    Make the Python example much prettier
    Signed-off-by: Vicent Marti <tanoku@gmail.com>

--- examples/example.py
+++ examples/example.py
@@ -3,18 +3,26 @@ import pyglet
 import ctypes
 from pyglet.gl import *
 
-window = pyglet.window.Window(visible=False)
+window_img = pyglet.window.Window(visible=False)
+window_depth = pyglet.window.Window(visible=False)
 
-@window.event
+@window_depth.event
 def on_draw():
-    background.blit_tiled(0, 0, 0, window.width, window.height)
-    frame.blit(0, 0, 0)
+    background.blit_tiled(0, 0, 0, w, h)
+    frame_depth.blit(0, 0, 0)
+
+@window_img.event
+def on_draw():
+    frame_img.blit(0, 0, 0)
 
 def kinect_tick(t):
     k.tick()
 
-    k.renderImage(texture, w)
-    frame.set_data("RGBA", w * -4, texture)
+    k.renderImage(texture_img, w)
+    frame_img.set_data("RGBA", w * -4, texture_img)
+
+    k.renderDepth(texture_depth, True, w)
+    frame_depth.set_data("RGBA", w * -4, texture_depth)
 
 def kinect_callback(cb_type, user_id):
     messages = [
@@ -31,7 +39,7 @@ if __name__ == '__main__':
     k = skeletonjelly.Kinect()
 
     k.setEventCallback(kinect_callback)
-    k.setTicksPerSecond(0)
+    k.setTicksPerSecond(30)
 
     k.init(skeletonjelly.Kinect.SENSOR_VGA_30FPS,
             skeletonjelly.Kinect.SENSOR_VGA_30FPS)
@@ -39,8 +47,11 @@ if __name__ == '__main__':
     w, h = k.getDepthResolution()
     size = k.getDepthTexSize()
 
-    texture = ctypes.create_string_buffer(size)
-    frame = pyglet.image.create(w, h)
+    texture_depth = ctypes.create_string_buffer(size)
+    texture_img = ctypes.create_string_buffer(size)
+
+    frame_depth = pyglet.image.create(w, h)
+    frame_img = pyglet.image.create(w, h)
 
     checks = pyglet.image.create(32, 32, pyglet.image.CheckerImagePattern())
     background = pyglet.image.TileableTexture.create_for_image(checks)
@@ -48,9 +59,13 @@ if __name__ == '__main__':
     glEnable(GL_BLEND)
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
-    window.width = w
-    window.height = h
-    window.set_visible()
+    window_depth.width = w
+    window_depth.height = h
+    window_depth.set_visible()
+
+    window_img.width = w
+    window_img.height = h
+    window_img.set_visible()
 
     pyglet.clock.schedule_interval(kinect_tick, 1.0 / 30.0)
     pyglet.app.run()
